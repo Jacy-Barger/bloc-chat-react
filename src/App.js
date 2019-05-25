424fb213eb683461b0b4429be23e74e53363de73
Jacy-Barger <barger.jacy@gmail.com>
2019-05-24 21:54:07 -0500

    Added the RoomList component and added firebase to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import RoomList from './components/RoomList'
+import * as firebase from 'firebase';
 
-function App() {
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+   apiKey: "AIzaSyCPI9oCmTR_0bj8UXcub2K1mu6Q9hXE6us",
+   authDomain: "bloc-chat-react-5f2d5.firebaseapp.com",
+   databaseURL: "https://bloc-chat-react-5f2d5.firebaseio.com",
+   projectId: "bloc-chat-react-5f2d5",
+   storageBucket: "bloc-chat-react-5f2d5.appspot.com",
+   messagingSenderId: "905849312906",
+   appId: "1:905849312906:web:993b0d2228ee8b66"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header>
-        <p>
-          Bloc Chat React
+        <p className="Header">
+          Bloc Chat
         </p>
       </header>
+        <div className="List">
+          <RoomList
+           firebase={firebase} />
+        </div>
     </div>
   );
+ }
 }
 
 export default App;
